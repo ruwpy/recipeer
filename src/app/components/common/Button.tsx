aa05b21cbc19ea74f8a73082ea49e396312c1b63
ruwpy <ruwpy.who@gmail.com>
2023-04-06 15:51:50 +0700

    add styles for button

--- src/app/components/common/Button.tsx
+++ src/app/components/common/Button.tsx
@@ -3,13 +3,31 @@ import { ReactNode } from "react";
 interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   disabled?: boolean;
+  buttonStyle?: "primary" | "accent";
 }
 
-const Button = ({ disabled, children, className, ...props }: ButtonProps) => {
+const Button = ({ disabled, children, buttonStyle, className, ...props }: ButtonProps) => {
+  let style;
+
+  switch (buttonStyle) {
+    case "primary":
+      style =
+        "border border-gray-200 hover:bg-gray-100 disabled:opacity-60 disabled:cursor-not-allowed";
+      break;
+
+    case "accent":
+      style =
+        "bg-blue-500 hover:bg-blue-600 text-white disabled:opacity-60 disabled:cursor-not-allowed";
+      break;
+
+    default:
+      style = "border border-gray-200 hover:bg-gray-100";
+      break;
+  }
   return (
     <button
       disabled={disabled}
-      className={`border border-gray-200 hover:bg-gray-100 w-fit h-10 px-4 rounded-lg ${className}`}
+      className={`w-fit h-10 px-4 transition-colors cursor rounded-lg ${style} ${className}`}
       {...props}
     >
       {children}

7e4dcdbf4b694ad6dbb09b3b90bb45c2bf47d962
ruwpy <ruwpy.who@gmail.com>
2023-04-06 15:45:59 +0700

    update types file

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,27 +1,34 @@
+import { Prisma } from "@prisma/client";
+
 export interface Recipe {
   id?: string;
   createdAt?: Date;
   updatedAt?: Date;
-  title?: string;
-  ingridients?: Ingredient[];
-  directions?: Direction[];
-  cookingTime?: number;
+  title: string;
+  description?: string;
+  categories?: Category[];
+  ingredients: Ingredient[];
+  directions: Direction[];
+  preparationTime: number;
+  amountOfServings: number;
+  cookingTime: number;
   authorId?: string;
 }
 
-export interface Ingredient {
+export interface Ingredient extends Prisma.InputJsonObject {
   id: string;
   value: string;
 }
 
-export interface Direction {
+export interface Direction extends Prisma.InputJsonObject {
   id: string;
   value: string;
   order: number;
 }
 
-export interface Category {
+export interface Category extends Prisma.InputJsonObject {
   id: string;
   name: string;
   recipes?: Recipe[];
+  userId?: string;
 }
